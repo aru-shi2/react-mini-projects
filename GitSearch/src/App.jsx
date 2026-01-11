019af5a7f5e4f62493a8b36dd950631dbef03164
aru-shi2 <arushimishra2929@gmail.com>
2026-01-11 21:40:49 +0800

    added search function

--- GitSearch/src/App.jsx
+++ GitSearch/src/App.jsx
@@ -1,12 +1,50 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Username, setUsername] = useState("")
+  const [Details, setDetails] = useState(null)
+
+    const fetchData=async () => {
+      if(Username){
+        const data=await fetch(`https://api.github.com/users/${Username}`)
+        const res=await data.json()
+        setDetails(res)
+      }
+      else{
+        <div>Not found!!</div>
+      }
+    }
+    
 
   return (
     <>
-      hi
+      <div className="box">
+        <div className="content">
+          <div className="input">
+            <input onChange={(e)=>setUsername(e.target.value)} value={Username} type="text" placeholder='Enter username...' />
+            <button onClick={fetchData} >Search</button>
+          </div>
+          <div className="data">
+            {Details && (
+               <div>
+                <div className="img">
+                  <img src={Details.avatar_url} alt="" />
+                </div>
+                <div className="content">
+                  {Details.name}
+                  {Details.login}
+                  {Details.bio}
+                  {Details.followers}
+                  {Details.following}
+                  {Details.public_repos}
+                  {Details.location}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
